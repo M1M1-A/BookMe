015b3db083425a4a44b2806682eac493f7abb0ae
Amina <amina.begum.01@gmail.com>
2024-01-25 14:04:32 +0000

    Home screen created and basic layout for DJ cards added

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,10 +1,48 @@
-import { View, Text } from "react-native";
+import { View, Text, StyleSheet, Image, TouchableOpacity, ScrollView } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 
 export default function Home() {
+
   return (
-    <View>
-      <Text>Hello World!</Text>
-    </View>
+    <SafeAreaView>
+      <ScrollView contentContainerStyle={styles.container}>
+        <View id="djCard" style={styles.djCard}>
+          {/* <Image source={}/> */}
+          <View id="djInfo">
+            <Text>Bio ajhdjahsjdhsja</Text>
+            <Text>Links, insta</Text>      
+          </View>
+          <TouchableOpacity style={styles.button}>
+            <Text>BOOK</Text>
+          </TouchableOpacity>
+        </View>
+      </ScrollView>    
+    </SafeAreaView>
   )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flexGrow: 1,
+    backgroundColor: "black",
+    justifyContent: 'center',
+    paddingVertical: 20
+  },
+  djCard: {
+    padding: 10,
+    alignSelf: 'center',
+    width: 400,
+    height: 650,
+    backgroundColor: "crimson",
+    borderRadius: 7
+  },
+  button: {
+    backgroundColor: '#f57c00',
+    height: 58,
+    borderRadius: 10,
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginTop: 40,
+  },
+})
