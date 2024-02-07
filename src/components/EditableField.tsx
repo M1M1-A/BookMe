67a86b70ab85efff6e385654484d97517f16c593
Amina <amina.begum.01@gmail.com>
2024-02-07 18:30:31 +0000

    Set up to have each field editable on its own

--- src/components/EditableField.tsx
+++ src/components/EditableField.tsx
@@ -2,7 +2,7 @@ import { View, Text, TextInput, TouchableOpacity, StyleSheet } from 'react-nativ
 import React, { useState } from 'react'
 import { SafeAreaView } from 'react-native-safe-area-context'
 
-const EditableField = ({ initialValue, placeholder, onSave}) => {
+const EditableField = ({ initialValue, label, onSave}) => {
   const [ value, setValue] = useState(initialValue)
   const [ editable, setEditable ] = useState(false)
 
@@ -12,29 +12,34 @@ const EditableField = ({ initialValue, placeholder, onSave}) => {
   }
 
   return (
-    <SafeAreaView style={styles.mainContainer}>
+    <View style={styles.mainContainer}>
       {editable ? (
-        <View style={styles.container}>
-          <TextInput
-            value={value}
-            placeholder={placeholder}
-            style={styles.inputField}
-            onChangeText={setValue}
-          />
-          <TouchableOpacity onPress={handleSave}>
-            <Text style={styles.inputField}>Save</Text>
-          </TouchableOpacity>
+        <View style={{flexDirection: 'column'}}>
+          <Text style={styles.label}>{label}</Text>
+          <View style={styles.container}>
+            <TextInput
+              value={value}
+              style={styles.inputField}
+              onChangeText={setValue}
+            />
+            <TouchableOpacity onPress={handleSave} style={styles.button}>
+              <Text style={styles.buttonText}>Save</Text>
+            </TouchableOpacity>
+          </View>
         </View>
         ) : (
-        <View style={styles.container}>
-          <Text style={styles.inputField}>{value}</Text>
-          <TouchableOpacity onPress={() => setEditable(true)}>
-            <Text style={styles.inputField}>Edit</Text>
-          </TouchableOpacity>
+        <View style={{flexDirection: 'column'}}> 
+          <Text style={styles.label}>{label}</Text>
+          <View style={styles.container}>
+            <Text style={styles.inputField}>{value}</Text>
+            <TouchableOpacity onPress={() => setEditable(true)} style={styles.button}>
+              <Text style={styles.buttonText}>Edit</Text>
+            </TouchableOpacity>
+          </View>
         </View>
         )   
       }
-    </SafeAreaView>
+    </View>
   )
 }
 
@@ -43,19 +48,44 @@ export default EditableField;
 const styles = StyleSheet.create({
   mainContainer: {
     display: 'flex',
-    // alignSelf: 'center'
+    alignItems: 'center', 
+    marginTop: 20,
+  },
+  label: {
+    alignSelf: 'flex-start', 
+    marginBottom: 5, 
+    fontSize: 18, 
+    // fontWeight: 'bold', 
   },
   container: {
     flexDirection: 'row',
     alignItems: 'center',
     justifyContent: 'center',
+    marginBottom: 10
   },
   inputField: {
-    fontSize: 20,
-    margin: 20,
-    fontWeight: 'bold'
+    backgroundColor: "#F7F8FB",
+    height: 40,
+    width: 280,
+    fontSize: 18,
+    borderRadius: 10,
+    fontWeight: 'bold',
+    padding: 10,
+    marginRight: 10
+  },
+  buttonText: {
+    fontWeight: "bold",
+    color: "#fff",
+    fontSize: 18,
+    alignItems: 'center'
   },
   button: {
+    height: 40,
+    width: 70,
     fontSize: 20,
+    backgroundColor: '#f57c00',
+    borderRadius: 5,
+    alignItems: 'center',
+    justifyContent: 'center'
   }
 })
--- src/screens/Profile.tsx
+++ src/screens/Profile.tsx
@@ -4,28 +4,35 @@ import { SafeAreaView } from 'react-native-safe-area-context'
 import EditableField from '../components/EditableField'
 
 const Profile = () => {
-  const [name, setName] = useState("Amina")
-  const [bio, setBio] = useState('hgjhgjhgkj')
+  const [name, setName] = useState("")
+  const [bio, setBio] = useState("")
+  const [images, setImages] = useState<UR[]>([])
+  const [audio, setAudio] = useState<URL>()
+  const [genres, setGenres] = useState<string[]>([])
+  const [instagramLink, setInstagramLink] = useState<string>("")
+  const [soundcloudLink, setSoundcloudLink] = useState<string>("")
+  const [availability, setAvailability] = useState("")
 
-  const handleSaveName = (newValue) => {
-    setName(newValue)
-  }
 
-  const handleSaveBio = (newValue) => {
-    setBio(newValue)
-  }
-
+  // Do I want one edit button per field, or one edit button 
+  // which makes all fields editable?
+  
   return (
     <SafeAreaView style={styles.mainContainer}>
       <EditableField 
         initialValue={name}
-        onSave={handleSaveName}
-        placeholder='Name'
+        onSave={setName}
+        label='Name'
       />
       <EditableField 
         initialValue={bio}
-        onSave={handleSaveBio}
-        placeholder='Bio'
+        onSave={setBio}
+        label='Bio'
+      />
+      <EditableField 
+        initialValue={images}
+        onSave={setImages}
+        label='Images'
       />
     </SafeAreaView>
   )
@@ -35,6 +42,7 @@ export default Profile
 
 const styles = StyleSheet.create({
   mainContainer: {
+    marginTop: 40,
     display: 'flex',
     alignSelf: 'center'
   },
